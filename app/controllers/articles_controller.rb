e33723c299cd167b9b2ff03e259cfefd55b58cd3
Ichnos <riccardo.decandia@gmail.com>
2016-02-21 18:28:03 +0000

    Association between user and article and debug view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    
+    @article.user = User.first
     if @article.save
       flash[:success] = "The article was successfully created!"
       redirect_to article_path(@article)
