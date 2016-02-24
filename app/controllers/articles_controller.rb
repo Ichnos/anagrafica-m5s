e3dd16be0cbb71b0a4c7c07e529e6f5db72e1235
Ichnos <riccardo.decandia@gmail.com>
2016-02-24 23:25:06 +0000

    Complete authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user,except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -13,24 +15,23 @@ class ArticlesController < ApplicationController
   def edit
   end
   
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:success] = "The article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
   def update
     if @article.update(article_params)
       flash[:success] = "The article was successfully updated!"
       redirect_to article_path(@article)
     else
       render 'edit'
-    end
-  end
-  
-  def create
-    
-    @article = Article.new(article_params)
-    @article.user = User.first
-    if @article.save
-      flash[:success] = "The article was successfully created!"
-      redirect_to article_path(@article)
-    else
-      render 'new'
     end
   end
   
@@ -42,8 +43,6 @@ class ArticlesController < ApplicationController
     flash[:danger] = "The article was successfully deleted"
     redirect_to articles_path
   end
-
-  
   
   private
     def set_article
@@ -53,5 +52,13 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+    
+    def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can edit or delete your own profile"
+      redirect_to root_path
+
+    end
+  end
   
 end
