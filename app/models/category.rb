2fbf536b5cf4b142ef1c444d670b7b434d7194cd
Ichnos <riccardo.decandia@gmail.com>
2016-03-07 20:33:02 +0000

    create associations between tables

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true,
              uniqueness: { case_sensitive: false }, # > validates_uniqueness_of :name   ||  è la stessa cosa
              length: { minimum: 3, maximum: 25 }
